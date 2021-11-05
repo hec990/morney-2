8d7f59ab91d67cedca0502e0c0148c870d202350
xiaohh <1296522554@qq.com>
2021-11-05 16:48:12 +0800

    初始化项目

---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+   记账
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Money"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+   统计
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Statistics"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
