497a46b7a8d6c5713489ce3a0de54f7c4103fb32
xiaohh <1296522554@qq.com>
2021-11-05 17:16:54 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from "vue-router";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,11 @@ const routes: Array<RouteConfig> = [
     {
         path: "/statistics",
         component: Statistics
+    },
+    {
+        // 404页面
+        path:"*",
+        component:NotFound
     }
 ];
 
