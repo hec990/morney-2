7d8abe35888c3623e2be8b106a77b9fc8fa8378d
xiaohh <1296522554@qq.com>
2021-11-07 22:27:18 +0800

    完成 Tags 组件

--- src/components/money-child/Tags.vue
+++ src/components/money-child/Tags.vue
@@ -1,38 +1,52 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource"
+          :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)"
+      >{{tag}}</li>
     </ul>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Tags'
-};
+import Vue from "vue";
+import {Component,Prop} from "vue-property-decorator";
+
+@Component
+export default class Tags extends Vue{
+ // string[] 是字符串数组，表示这个数组里只能存字符串
+ // readonly 是只读的意思，因为dataSource是外部传来的，避免我们自己在子组件修改父组件的数据，如果子组件有修改数据的动作会提示这个数据是只读的，不能修改。
+ @Prop() readonly dataSource: string[] | undefined;
+  // 选中的tag
+  selectedTags: string[] = [];
+
+  toggle(tag: string) {
+    // 获取到选中的tag
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      // 二次点击取消tag，如果当前tag被选中就删除
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
+
+  create() {
+    const name = window.prompt("请输入标签名");
+    if (name === "") {
+      window.alert("标签名不能为空");
+    } else if (this.dataSource) {
+      // 将最新的数据发送给外面
+      this.$emit("update:dataSource",[...this.dataSource, name]);
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -46,7 +60,8 @@ export default {
     display: flex;
     flex-wrap: wrap;
     > li {
-      background: #d9d9d9;
+      $bg: #d9d9d9;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -54,6 +69,10 @@ export default {
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected {
+        background: darken($bg, 50%);
+        color: white;
+      }
     }
   }
   > .new {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <Layout class-prefix="layout">
-    <Tags/>
+    <Tags :data-source.sync="tags"/>
     <Notes/>
     <Types/>
     <NumberPad/>
@@ -9,14 +9,19 @@
 </div>
 </template>
 
-<script lang="ts">
+<script>
 import Tags from "@/components/money-child/Tags.vue";
 import Notes from "@/components/money-child/Notes.vue";
 import Types from "@/components/money-child/Types.vue";
 import NumberPad from "@/components/money-child/NumberPad.vue";
 export default {
   name: "Money",
-  components: {NumberPad, Types, Notes, Tags}
+  components: {NumberPad, Types, Notes, Tags},
+  data(){
+    return {
+      tags:['衣','食','住','行','玩']
+    }
+  }
 };
 </script>
 
