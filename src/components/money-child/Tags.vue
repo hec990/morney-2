abb3cde3f9681947562e2514b935a016dcd780f6
xiaohh <1296522554@qq.com>
2021-11-09 21:43:20 +0800

    解决tags 渲染问题

--- src/components/money-child/Tags.vue
+++ src/components/money-child/Tags.vue
@@ -7,8 +7,8 @@
       <li v-for="tag in dataSource"
           :key="tag.id"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag.name)"
-      >{{tag}}</li>
+          @click="toggle(tag)"
+      >{{tag.name}}</li>
     </ul>
   </div>
 
