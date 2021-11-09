519ec0bddc34315966a6f09dc08cd3adf028e576
xiaohh <1296522554@qq.com>
2021-11-09 21:27:24 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -2,17 +2,17 @@ import RecordItem from "@/custom";
 
 const localStorageKeyName = 'recordList';
 const recordListModel = {
-    // 克隆
+    data: [] as RecordItem[],
     clone(data: RecordItem[] | RecordItem) {
         return JSON.parse(JSON.stringify(data));
     },
-    // 获取
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.data;
     },
-    // 保存
-    save(data: RecordItem[]) {
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+
     }
 };
 
