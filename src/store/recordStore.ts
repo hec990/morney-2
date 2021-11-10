75fcab893c5a1ffae9de24a78cbb5bf6f80810f7
xiaohh <1296522554@qq.com>
2021-11-10 22:49:42 +0800

    删除之前自己做的全局数据管理

--- src/store/recordStore.ts
+++
@@ -1,7 +0,0 @@
-import recordListModel from '@/models/recordListModel';
-
-export default  {
-    // record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.create(record),
-}
